64f67c46f6f97797d9b758094690cc01f0ca053c
sentient-codebot-nan <linnunium@live.com>
2021-11-19 17:18:29 +0100

    new dataset type

--- Data.py
+++ Data.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import os
 import pandas as pd
+import numpy as np
 from torchvision.io import read_image
 
 class COVID19Daily(Dataset):
@@ -19,9 +20,29 @@ class COVID19Daily(Dataset):
 
         return torch.tensor(past_seq).float(), torch.tensor(present).reshape(1).float()
 
+class COVID19Average(Dataset):
+    '''
+    x: past p days
+    y: average of future 7 seven days (including today)
+    '''
+    def __init__(self, csv_file, segment_length):
+        self.df = pd.read_csv(csv_file)
+        self.segment_length = segment_length
+
+    def __len__(self):
+        return len(self.df)-self.segment_length-6 # reserve the last as the last LABEL
+
+    def __getitem__(self, idx):
+        ''' idx: starting index '''
+        past_seq = self.df.iloc[idx:idx+self.segment_length, -1].to_numpy()
+        pre_2_future = self.df.iloc[idx+self.segment_length:idx+self.segment_length+7, -1].to_numpy() # series, already numpy (?)
+        average_future = np.mean(pre_2_future)
+
+        return torch.tensor(past_seq).float(), torch.tensor(average_future).reshape(1).float()
+
 def main():
     filename = "data/COVID-19_aantallen_nationale_per_dag.csv"
-    dataset = COVID19Daily(filename, 10)
+    dataset = COVID19Average(filename, 10)
     get = next(iter(dataset))
     print(f"DATASET returned item: {get[0].shape}, {get[1].shape}")
 
